5e986a6c483ba70fe2049033212867bf2ebdb503
whgms159 <whgms159@naver.com>
2024-07-16 19:10:22 +0900

    solve : [BOJ] 별 찍기-2_240716

--- week03/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ week03/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int i=0;
+		int i=0; 
 			while (i < n) {
 				System.out.println(" ".repeat(n - (i + 1)) + "*".repeat(n - (n - (i + 1))));
 				i++;
