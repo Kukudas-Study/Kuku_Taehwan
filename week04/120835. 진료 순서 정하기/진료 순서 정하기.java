b7e8a3c3c61c8df378d26499ded71264d152028c
whgms159 <whgms159@naver.com>
2024-07-23 18:58:31 +0900

    solve : [PRO] 진료 순서 정하기_240723

--- week04/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ week04/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] solution(int[] emergency) {
-        int[] arr2 = new int[emergency.length];
-        for(int i =0; i<emergency.length; i++) {
-			int count =0;
-			for(int j = 0; j<emergency.length; j++) {
+        int[] arr2 = new int[emergency.length]; // 주어진 배열
+        for(int i =0; i<emergency.length; i++) { //for문 두 개를 이용한 비교
+			int count =0;		//진료순서 count
+			for(int j = 0; j<emergency.length; j++) {d
 				if(emergency[i]<emergency[j]) {
 					count++;
 					arr2[i] = count;
 				}
-			}arr2[i]++;
+			}arr2[i]++; // 진료순서 1씩 추가
         }
         return arr2;
     }
