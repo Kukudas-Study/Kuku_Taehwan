5efa83f34c8dc838fdd7725dc0ab30b86810cead
whgms159 <whgms159@naver.com>
2024-07-29 13:52:29 +0900

    solve : [BOJ] 소트인사이드_240729

--- week05/1427. 소트인사이드/소트인사이드.java
+++ week05/1427. 소트인사이드/소트인사이드.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String word = sc.nextLine();
+		String word = sc.nextLine(); //수를 문자열로 받음
 		
 		int[] arr = new int[word.length()];
 		
 		for(int i =0; i<word.length();i++) {
-			arr[i]= Integer.parseInt(String.valueOf(word.charAt(i)));
+			arr[i]= Integer.parseInt(String.valueOf(word.charAt(i)));//문자열을 정수로 변환 후 대입
 		}
-		Arrays.sort(arr);
-		for(int i=0; i<arr.length/2;i++) {
+		Arrays.sort(arr);//배열 정렬
+		for(int i=0; i<arr.length/2;i++) {//정렬된 의 반만큼 재배열
 			int tmp = arr[i];
 			arr[i] = arr[arr.length-i-1];
 			arr[arr.length-i-1] = tmp;
