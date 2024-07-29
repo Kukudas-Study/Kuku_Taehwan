9e83d867f7821701ec715f74243bd85ba4345ef6
whgms159 <whgms159@naver.com>
2024-07-29 18:55:07 +0900

    solve : [BOJ]세로읽기_240729

---
+++ week05/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);		
+		char[][] arr = new char[5][15];
+		
+		for(int i =0; i<5; i++) {
+			String word = sc.nextLine(); 
+			for(int j =0; j<word.length(); j++) {
+				arr[i][j] = word.charAt(j);
+			}
+		}
+		
+		for(int i=0; i<15; i++) {
+			for(int j =0; j<5; j++) {
+				if(arr[j][i] ==0) {
+					continue;
+				}
+				System.out.print(arr[j][i]);
+			}
+		}
+	}
+
+}
