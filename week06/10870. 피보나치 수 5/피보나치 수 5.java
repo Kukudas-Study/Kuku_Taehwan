50142c568f934999ddcf2a674c568027463bd597
whgms159 <whgms159@naver.com>
2024-08-06 20:00:48 +0900

    a

---
+++ week06/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum = 0;
+		int a = 1;
+		int b = 0;
+		for (int i = 0; i < n; i++) {
+			sum = a + b;
+			a = b;
+			b = sum;
+		}
+		System.out.println(sum);
+	}
+
+}
---
+++ week06/1343. 폴리오미노/폴리오미노.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);		
+		
+//		String N = sc.nextLine().replace("XXXX", "AAAA").replace("XX", "BB");
+//		if(N.contains("X")) {
+//			N="-1";
+//		}
+		String N = sc.nextLine();
+		for(int i=0; i<N.length(); i++) {
+			if(N.contains("XXXX") || N.contains("XX")) {
+				N =N.replace("XXXX", "AAAA");
+				N =N.replace("XX", "BB");
+			}
+		}	
+		
+		if(N.contains("X")) {
+			N="-1";
+		}
+		System.out.println(N);
+		
+	}
+
+}
